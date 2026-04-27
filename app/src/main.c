438a655b16631fddef40009a3e8e237808e4a544
Omid Kandelusy <omidkandelusy@gmail.com>
2026-04-27 10:49:38 +0200

    feat: populates a connection manager layer

--- app/src/main.c
+++ app/src/main.c
@@ -185,18 +185,8 @@ BT_GATT_SERVICE_DEFINE(vnd_svc,
 			       write_without_rsp_vnd, &vnd_wwr_value),
 );
 
-static const struct bt_data ad[] = {
-	BT_DATA_BYTES(BT_DATA_FLAGS, (BT_LE_AD_GENERAL | BT_LE_AD_NO_BREDR)),
-	BT_DATA_BYTES(BT_DATA_UUID16_ALL,
-		      BT_UUID_16_ENCODE(BT_UUID_HRS_VAL),
-		      BT_UUID_16_ENCODE(BT_UUID_BAS_VAL),
-		      BT_UUID_16_ENCODE(BT_UUID_CTS_VAL)),
-	BT_DATA_BYTES(BT_DATA_UUID128_ALL, BT_UUID_CUSTOM_SERVICE_VAL),
-};
 
-static const struct bt_data sd[] = {
-	BT_DATA(BT_DATA_NAME_COMPLETE, CONFIG_BT_DEVICE_NAME, sizeof(CONFIG_BT_DEVICE_NAME) - 1),
-};
+
 
 void mtu_updated(struct bt_conn *conn, uint16_t tx, uint16_t rx)
 {
@@ -207,19 +197,7 @@ static struct bt_gatt_cb gatt_callbacks = {
 	.att_mtu_updated = mtu_updated
 };
 
-static void connected(struct bt_conn *conn, uint8_t err)
-{
-	if (err) {
-		printk("Connection failed, err 0x%02x %s\n", err, bt_hci_err_to_str(err));
-	} else {
-		printk("Connected\n");
-	}
-}
 
-static void disconnected(struct bt_conn *conn, uint8_t reason)
-{
-	printk("Disconnected, reason 0x%02x %s\n", reason, bt_hci_err_to_str(reason));
-}
 
 static void alert_stop(void)
 {
@@ -236,10 +214,7 @@ static void alert_high_start(void)
 	printk("High alert started\n");
 }
 
-BT_CONN_CB_DEFINE(conn_callbacks) = {
-	.connected = connected,
-	.disconnected = disconnected,
-};
+
 
 BT_IAS_CB_DEFINE(ias_callbacks) = {
 	.no_alert = alert_stop,
@@ -247,24 +222,7 @@ BT_IAS_CB_DEFINE(ias_callbacks) = {
 	.high_alert = alert_high_start,
 };
 
-static void bt_ready(void)
-{
-	int err;
 
-	printk("Bluetooth initialized\n");
-
-	if (IS_ENABLED(CONFIG_SETTINGS)) {
-		settings_load();
-	}
-
-	err = bt_le_adv_start(BT_LE_ADV_CONN_FAST_1, ad, ARRAY_SIZE(ad), sd, ARRAY_SIZE(sd));
-	if (err) {
-		printk("Advertising failed to start (err %d)\n", err);
-		return;
-	}
-
-	printk("Advertising successfully started\n");
-}
 
 static void auth_passkey_display(struct bt_conn *conn, unsigned int passkey)
 {
@@ -276,11 +234,6 @@ static void auth_cancel(struct bt_conn *conn)
 	printk("Pairing cancelled: %s\n", bt_conn_dst_str(conn));
 }
 
-static struct bt_conn_auth_cb auth_cb_display = {
-	.passkey_display = auth_passkey_display,
-	.passkey_entry = NULL,
-	.cancel = auth_cancel,
-};
 
 static void bas_notify(void)
 {
@@ -396,18 +349,12 @@ int main(void)
 	char str[BT_UUID_STR_LEN];
 	int err;
 
-	err = bt_enable(NULL);
-	if (err != 0) {
-		printk("Bluetooth init failed (err %d)\n", err);
-		return 0;
-	}
 
-	bt_ready();
 	bt_cts_init(&cts_cb);
 	bt_hrs_cb_register(&hrs_cb);
 
 	bt_gatt_cb_register(&gatt_callbacks);
-	bt_conn_auth_cb_register(&auth_cb_display);
+	
 
 	vnd_ind_attr = bt_gatt_find_by_uuid(vnd_svc.attrs, vnd_svc.attr_count,
 					    &vnd_enc_uuid.uuid);
